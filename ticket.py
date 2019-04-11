3da508a5e717fcce640382c3817cebd8df7ee409
nimisha026 <49266248+nimisha026@users.noreply.github.com>
2019-04-11 12:50:47 +0530

    8th solution

---
+++ ticket.py
@@ -0,0 +1,17 @@
+a=eval(input('enter speed'))
+x=eval(input('if today is your bday press 1 else print 2'))
+if(x==1):
+ if(a>=85):
+   print('ticket 1')
+ elif(a>=65 & a<85):
+   print('ticket 2')
+ elif(a<65):
+   print('ticket 3')
+elif(x==2):
+ if(a>=80):
+   print('ticket 1')
+ elif(a>=60 & a<80):
+   print('ticket 2')
+ elif(a<60):
+   print('ticket 3')
+
